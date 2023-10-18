8ba5e38085afae86ee8a8c3e27e2c4a5a7635c79
= <elvinhndrwn@gmail.com>
2023-10-19 02:22:08 +0700

    init commit

---
+++ src/test/java/com/azure/springbootazuredemo/SpringbootAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.springbootazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
